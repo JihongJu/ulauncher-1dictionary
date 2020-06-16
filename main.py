dba36155502b85c6fb5040a8142614525d11d0ad
Jihong Ju <jihong.ju@tomtom.com>
2020-06-16 10:01:44 +0200

    better hints before typing

--- main.py
+++ main.py
@@ -112,6 +112,13 @@ class KeywordQueryEventListener(EventListener):
                     description="Language: {}".format(language),
                     on_enter=OpenUrlAction(dictionaries.get(language, DEFAULT_DICTIONARY) % word)))
 
+        else:
+            items.append(ExtensionResultItem(
+                icon='images/icon.png',
+                name="Type in the word...",
+                description="",
+                ))
+
         return RenderResultListAction(items)
 
 
